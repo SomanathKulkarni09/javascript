b8b7575e2ac0c08d7afa24345b67e63d71d4dd2c
Somanath Kulkarni <somanathkulkarni35@gmail.com>
2026-03-16 19:26:32 +0530

    inheritance & method overriding

--- learning/Advance/myClass.js
+++ learning/Advance/myClass.js
@@ -16,13 +16,26 @@
         this.lastname = myname[2]
     }
 
+}
 
- }
-
- const john = new User("John","Marks","Anderson",30);
+class Teacher extends User{
+    constructor(firstname,middlename, lastname, credit,subject){
+        super(firstname,middlename, lastname, credit);
+        this.subject = subject;
+    }
+    getFullName() {
+    return `${this.firstname} ${this.middlename} ${this.lastname} is my full name and I tech ${this.subject}`;
+    }
+    favDrink(drink){
+        console.log(`My fav drink is ${drink}`);
+        
+    }
+}
+ const john = new Teacher("John","Marks","Anderson",30,'Python');
  
  console.log(john.getFullName());
 
- john.editName("Jonny Samyyy Anderson");
+//  john.editName("Jonny Samyyy Anderson");
 
-  console.log(john.getFullName());
+//   console.log(john.getFullName());
+john.favDrink("Tea");
